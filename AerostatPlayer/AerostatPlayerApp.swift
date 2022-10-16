b13747ec867c0849a5a5ecaf141877036df4a241
Yuri Andrianov <andrianov.yuri.spb@gmail.com>
2022-10-16 16:04:19 +0400

    Initial Commit

---
+++ AerostatPlayer/AerostatPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerostatPlayerApp.swift
+//  AerostatPlayer
+//
+//  Created by Yuri Andrianov on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AerostatPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
