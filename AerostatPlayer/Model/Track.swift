eab022e5be0ccd9d27432fa5be537399508e9a4a
Yuri Andrianov <andrianov.yuri.spb@gmail.com>
2022-10-17 19:10:18 +0400

    change project structure

---
+++ AerostatPlayer/Model/Track.swift
@@ -0,0 +1,14 @@
+//
+//  Track.swift
+//  AerostatPlayer
+//
+//  Created by Yuri Andrianov on 17.10.2022.
+//
+
+import SwiftUI
+
+struct Track: Identifiable {
+    
+    var id = UUID()
+    let name: String
+}
--- AerostatPlayer/Views/ContentView.swift
+++ AerostatPlayer/Views/ContentView.swift
@@ -7,24 +7,6 @@
 
 import SwiftUI
 
-struct Track: Identifiable {
-    
-    var id = UUID()
-    let name: String
-}
-
-class ViewModel: ObservableObject {
-    
-    @Published var tracks: [Track] = [
-        Track(name: "СЛОТ - Москва.mp3"),
-        Track(name: "DMX - Party up.mp3"),
-        Track(name: "Modern Talking - You're my heart you're my soul.mp3"),
-        Track(name: "MC Hammer - U can't touch this.mp3"),
-        Track(name: "Enya - Only time.mp3"),
-        Track(name: "Adriano Celentano - I love you baby.mp3")
-    ]
-}
-
 struct ContentView: View {
     
     @ObservedObject var viewModel = ViewModel()
